db774326cea3d82381fa1933378b83e8988a4791
garewoodadmin <garewoodinfra@gmail.com>
2025-11-25 22:05:06 +0000

    Remove build artifacts from git tracking and fix double to int conversion warning

--- build/CPM_modules/FindCatch2.cmake
+++
@@ -1,3 +0,0 @@
-include("/home/smellor/projects/audio/delay_crafter/build/cmake/CPM_0.40.2.cmake")
-CPMAddPackage("GITHUB_REPOSITORY;catchorg/Catch2;VERSION;3.8.1;EXCLUDE_FROM_ALL;YES;SYSTEM;YES;")
-set(Catch2_FOUND TRUE)
